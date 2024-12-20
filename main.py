a28277407e0c5cefa27504e6b20c55db2f338127
Daniel Kriz <147802055+Daniel908009@users.noreply.github.com>
2024-12-20 19:49:19 +0100

    A lot of new features and functions.

--- main.py
+++ main.py
@@ -14,14 +14,16 @@ class SettingsPopup(Popup):
         # code for applying settings will be added later
         self.dismiss()
 
+class ResultsPopup(Popup):
+    pass
+
 class Tile(Button):
     def __init__(self, caller, id):
         super(Tile, self).__init__()
         self.caller = caller
         self.id = id
     def click(self):
-        #print("tile clicked at id: ", self.id)
-        pass
+        pass # maybe I will add something here later
 
 class MainGrid(GridLayout):
     def __init__(self):
@@ -42,16 +44,12 @@ class MainGrid(GridLayout):
     def keyAction(self, instance, keyboard, keycode, text, modifiers):
         if self.ids.startQuitButton.text == "Quit": # the direction can only be changed when the game is running
             if text == 'w' and self.snakeDirection != "down":
-                #print("w pressed")
                 self.snakeDirection = "up"
             elif text == 'a' and self.snakeDirection != "right":
-                #print("a pressed")
                 self.snakeDirection = "left"
             elif text == 's' and self.snakeDirection != "up":
-                #print("s pressed")
                 self.snakeDirection = "down"
             elif text == 'd' and self.snakeDirection != "left":
-                #print("d pressed")
                 self.snakeDirection = "right"
     def openSettings(self):
         popup = SettingsPopup(self)
@@ -61,7 +59,6 @@ class MainGrid(GridLayout):
         minutes, seconds = divmod(self.time, 60)
         self.ids.timeLabel.text = 'Time: {:02}:{:02}'.format(minutes, seconds)
     def startGame(self):
-        #print("Game button pressed")
         if self.ids.startQuitButton.text == "Start":
             self.ids.startQuitButton.text = "Quit"
             self.ids.startQuitButton.background_color = (1, 1, 0, 1)
@@ -72,39 +69,29 @@ class MainGrid(GridLayout):
             self.ids.startQuitButton.background_color = (0, 1, 0, 1)
             self.timeClockEvent.cancel()
             self.gameClockEvent.cancel()
-            # results popup will be added later
+            popup = ResultsPopup() # will be enhanced later
+            popup.open()
             self.resetGame()
     def gameLoop(self, t):
         # the movement logic
         if self.snakeDirection != '':
             if self.snakeDirection == "up":
-                #print("up")
                 self.moveSnake("up")
             elif self.snakeDirection == "left":
-                #print("left")
                 self.moveSnake("left")
             elif self.snakeDirection == "down":
-                #print("down")
                 self.moveSnake("down")
             elif self.snakeDirection == "right":
-                #print("right")
                 self.moveSnake("right")
-        
-        #print(self.food)
-        #print(len(self.food))
-        #print(self.numberOfFood)
-        
         # all the food logic
         if self.numberOfFood > len(self.food):
             self.generateFood()
-            #print("food generated")
     def moveSnake(self, direction):
         for part in self.snake:
             for tile in self.ids.snakeGrid.children:
                 if tile.id == part:
                     tile.background_color = (1, 1, 1, 1)
         copySnake = []
-        #print(len(self.snake))
         for i in self.snake:
             copySnake.append(i)
         if direction == "up":
@@ -129,7 +116,6 @@ class MainGrid(GridLayout):
                     self.snake[i] = copySnake[i-1]
         else:
             self.snakeDirection = "up"
-
         for part in self.snake:
             for tile in self.ids.snakeGrid.children:
                 if tile.id == part:
@@ -139,9 +125,17 @@ class MainGrid(GridLayout):
                 self.snake.append(apple[0]*self.sizeOfGrid+apple[1])
                 self.food.remove(apple)
                 self.score += 1
-                #print(self.score)
                 self.ids.scoreLabel.text = 'Score: {}'.format(self.score)
-                #rint("Apple eaten")
+        self.checkCollision()
+    def checkCollision(self):
+        if self.snake[0] < 0 or self.snake[0] >= self.sizeOfGrid*self.sizeOfGrid:
+            self.startGame() # calling the startGame function because this will open the results popup
+        #print(self.snake)
+        # checking sides is not really possible because of the way I made the board, however I dont think it is necessary, will try to fix it later if I come up with something
+        for i in range(len(self.snake)):
+            if i != 0:
+                if self.snake[0] == self.snake[i]:
+                    self.startGame()
     def generateFood(self):
         coords =[random.randint(0, self.sizeOfGrid-1), random.randint(0, self.sizeOfGrid-1)]
         while coords in self.snake:
@@ -151,7 +145,6 @@ class MainGrid(GridLayout):
             if tile.id == coords[0]*self.sizeOfGrid+coords[1]:
                 tile.background_color = (1, 0, 0, 1)
     def resetGame(self):
-        #print("Game reset")
         self.time = 0
         self.ids.timeLabel.text = 'Time: 00:00'
         self.score = 1
